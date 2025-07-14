e136751abae81eae090c4d461ad042004ac5d3f3
Nanonets <12345678+ashutalewar@users.noreply.github.com>
2025-07-14 11:38:01 +0530

    Bump version to 0.4.0

--- llm_converter/__init__.py
+++ llm_converter/__init__.py
@@ -6,5 +6,5 @@ from .converter import FileConverter
 from .result import ConversionResult
 from .exceptions import ConversionError, UnsupportedFormatError
 
-__version__ = "0.1.0"
+__version__ = "0.4.0"
 __all__ = ["FileConverter", "ConversionResult", "ConversionError", "UnsupportedFormatError"] 
